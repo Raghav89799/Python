daa73ce627b00febf6e6294746d43122f717abd5
Raghav89799 <132428418+Raghav89799@users.noreply.github.com>
2023-09-08 19:54:50 +0530

    Arithmetic_Operators

---
+++ Arithmetic_Operator.py
@@ -0,0 +1,55 @@
+# Program for addition
+
+a = 67
+b = 45
+
+print(a+b)
+
+# Program for addition from user input
+
+a = int(input("Enter the Value of First num : "))
+b = int(input("Enter the Value of Second num : "))
+
+print(a+b)
+
+# Program for subtraction
+
+a = 56
+b = 8
+
+print(a-b)
+
+# Program for Subtraction from user input
+
+a = int(input("Enter the Value of First num : "))
+b = int(input("Enter the Value of Second num : "))
+
+print(a-b)
+
+# Program for multiplication
+
+a = 6
+b = 4
+
+print(a*b)
+
+# Program for Multiplication from user input
+
+a = int(input("Enter the Value of First num : "))
+b = int(input("Enter the Value of Second num : "))
+
+print(a*b)
+
+# Program for division
+
+a = 78
+b = 5
+
+print(a/b)
+
+# Program for division from user input
+
+a = int(input("Enter the Value of First num : "))
+b = int(input("Enter the Value of Second num : "))
+
+print(a/b)
