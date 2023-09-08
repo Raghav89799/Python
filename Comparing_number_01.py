b7d5fee055f4799d34a3ff125c081cb53c2ba542
Raghav89799 <132428418+Raghav89799@users.noreply.github.com>
2023-09-08 20:29:21 +0530

    Comparing 2 numbers

---
+++ Comparing_number_01.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter the First Number value : "))
+num2 = int(input("Enter the Second Number value : "))
+
+if num1 > num2:
+    print(num1,"is greater than",num2)
+elif num1 < num2:
+    print(num2,"is greater than",num1)
+else:
+    print(num1,"is equal to",num2)
