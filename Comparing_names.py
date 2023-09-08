a09cfe2a203ffe288c5723202e3c1b448ab997fb
Raghav89799 <132428418+Raghav89799@users.noreply.github.com>
2023-09-08 21:16:19 +0530

    Comparing names

---
+++ Comparing_names.py
@@ -0,0 +1,7 @@
+name_01 = input("Enter the first name : ")
+name_02 = input("Enter the Second name : ")
+
+if name_01.lower() == name_02.lower():
+    print("Both names are same")
+else:
+    print("Both names are not same")
